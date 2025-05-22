6bffa367abcba709d880f76f49642cb6e93c8b0c
InstincT <113701884+monish-instinct@users.noreply.github.com>
2025-05-22 16:52:22 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .binaryTarget(
             name: "MyNeuralFramework",
             url: "https://github.com/monish-instinct/SampleXC/releases/download/samplexc/MyNeuralFramework.xcframework.zip",
-            checksum: "6f8cd67c40b919984ca9bf648f4432a28d2e4f9296e8145ea4c2b5042aa80e71"
+            checksum: "1526c5c9906da0fa7185522c47170b5293d699a916ff5350da218c4f86c159cb"
         )
     ]
 )
